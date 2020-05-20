03bafaddf1128517b12057457f547f2ebc824ecb
Raimundo Luiz Oliveira <rayfcrols@gmail.com>
2020-05-20 17:12:57 -0300

    Updata

--- .react-playground/index.js
+++
@@ -1,8 +0,0 @@
-
-        import React from 'react';
-        import ReactDom from 'react-dom';
-        import Playground from 'react-playground-kit';
-        import * as module from '/home/raimundo/devolps/rocketseal/omnistack11/frontend/src/services/api.js';
-
-        ReactDom.render(React.createElement(Playground, { module }), document.getElementById('app'));
-    
